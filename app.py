e701d082532dd8972e0080ed0fe101ec7771a5b8
clooooode <jackey8616@gmail.com>
2018-10-03 01:15:47 +0800

    Add tmp folder auto create

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-import click
+import os, click
 from sanic import Sanic
 from sanic.response import json
 from sanic_cors import CORS, cross_origin
@@ -12,12 +12,17 @@ app = None
 @click.option('--host', default='0.0.0.0', type=str)
 @click.option('--port', default=8000, type=int)
 def run(host, port):
+    path = os.path.join(os.getcwd(), 'tmp/')
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     global app
     app = Sanic()
     CORS(app)
     app.add_route(index, '/', methods=['GET'])
     stage_add_route(app)
     app.run(host=host, port=port)
+
 
 async def index(request):
     return json({ 'success': True, 'data': 'Hello World!' })
