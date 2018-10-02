778e55b572841bab0de2305c14b50192d3fa9d8a
clooooode <jackey8616@gmail.com>
2018-10-03 00:44:58 +0800

    Heroku

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ async def index(request):
     return json({ 'success': True, 'data': 'Hello World!' })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0')
