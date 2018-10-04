0a4c16058f7cf7478a61d4335545ad6f73a7958a
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2018-10-04 19:38:05 +0530

    Create s6_list_eexample.py

---
+++ stage/one/s6_list_eexample.py
@@ -0,0 +1,15 @@
+# Declaring and printing list 
+Lst = [1, "a" , "string" , 1+2] 
+print(L) 
+  
+# Adding more items 
+Lst.append(6) 
+print(L) 
+  
+# Removing an item 
+Lst.pop() 
+  
+print(Lst) 
+  
+# Printing a particular item 
+print(Lst[1]) 
