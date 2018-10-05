0b7c71f19ee6ff6aa128946547f7f215ee18a1bd
claudiavmbrito <cvmb.mat@hotmail.com>
2018-10-05 17:31:00 +0100

    Update test_s9_if.py

--- tests/stage/one/test_s9_if.py
+++ tests/stage/one/test_s9_if.py
@@ -4,7 +4,7 @@ import json
 
 async def test_if(test_cli):
     req_data = json.dumps({
-        'field_1': 'x>18'})
+        'field_1': 'age>18'})
     
     res = await test_cli.post('/stage/one/if', data=req_data)
     assert res.status == 200
