8824f935e1d08b610a5a15df6607b6457ad77cde
claudiavmbrito <cvmb.mat@hotmail.com>
2018-10-05 17:58:57 +0100

    Added if lesson in stage one (#20)

---
+++ tests/stage/one/test_s10_if.py
@@ -0,0 +1,13 @@
+from stage.one.s10_if import route, data
+from tests.utils import check_attributes, post
+
+
+def test_attributes():
+    check_attributes(route, data)
+
+
+async def test_lesson(test_cli):
+    req_data = {
+        'field_1': 'age>18',
+    }
+    await post(test_cli, route['url'], data=req_data)
