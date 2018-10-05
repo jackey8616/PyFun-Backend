0a5843ae72ec7f3bffe167527789a4842ff11cdc
claudiavmbrito <cvmb.mat@hotmail.com>
2018-10-05 17:08:08 +0100

    Add files via upload

---
+++ tests/stage/one/test_s9_if.py
@@ -0,0 +1,11 @@
+
+import json
+
+
+async def test_if(test_cli):
+    req_data = json.dumps({'field_1': 'x>18'})
+    res = await test_cli.post('/stage/one/if', data=req_data)
+    assert res.status == 200
+    res_data = await res.json()
+    assert res_data['data']['result'] is True
+    await test_cli.close()
