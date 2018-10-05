a26a46f6c36d75ac2f8b00340fdb40a2c4745bf0
claudiavmbrito <cvmb.mat@hotmail.com>
2018-10-05 17:35:19 +0100

    Update s9_if.py

--- stage/one/s9_if.py
+++ stage/one/s9_if.py
@@ -24,7 +24,7 @@ data = {
     ],
     'code': [
         'age=20',
-        'if(_____):',
+        'if(______):',
         '   print(\'You are old\')',
         'else:'
         '   print(\'You are young\')',
