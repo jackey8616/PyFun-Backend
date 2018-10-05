6c8b116ae0a3b7a3a446cba442c54f3f3871d587
claudiavmbrito <cvmb.mat@hotmail.com>
2018-10-05 17:29:40 +0100

    Update test_s9_if.py

--- tests/stage/one/test_s9_if.py
+++ tests/stage/one/test_s9_if.py
@@ -4,11 +4,9 @@ import json
 
 async def test_if(test_cli):
     req_data = json.dumps({
-        'field_1': 'x',
-        'field_2': '>',
-        'field_3': '18'})
+        'field_1': 'x>18'})
     
-    res = await test_cli.post('/stage/two/list_comprehensions', data=req_data)
+    res = await test_cli.post('/stage/one/if', data=req_data)
     assert res.status == 200
     res_data = await res.json()
     assert res_data['data']['result'] is True
