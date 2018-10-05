7b2e0c2e42ce998ca5b75bdb5de42434b1a74c09
Tim Burke <tim.burke@gmail.com>
2018-10-05 11:49:54 -0700

    Clean up stage/one/if
    Previously, we were joining words together in the description, and the
    indentation for the sample code wasn't sufficient to trigger the
    &nbsp;-formatting.

--- stage/one/s10_if.py
+++ stage/one/s10_if.py
@@ -12,22 +12,22 @@ data = {
     'title': 'Python Strings',
     'description': [
         'Lets get familiar with Python If-Conditions!',
-        'As the name indicates, If-conditions work as' +
+        'As the name indicates, If-conditions work as ' +
         'they are named, "If something happened, then something else happens".',
         'You can reference if statements here: https://docs.python.org/3/' +
         'tutorial/controlflow.html',
         '\n',
-        'Now lets work on some exercises. A person is 20 year old age' +
+        'Now lets work on some exercises. A person is 20 year old age ' +
         'is she young or old?',
-        'Create a if statement that prints that if a person is older than' +
+        'Create a if statement that prints that if a person is older than ' +
         '18 years old is old, otherwise the person is young.'
     ],
     'code': [
-        'age=20',
-        'if(_____):',
-        '   print(\'You are old\')',
-        'else:'
-        '   print(\'You are young\')',
+        'age = 20',
+        'if (_____):',
+        '    print(\'You are old\')',
+        'else:',
+        '    print(\'You are young\')',
     ],
     'fields': []
 }
@@ -49,7 +49,7 @@ def answer(stdout, stderr):
             return False
         else:
             ans = "You are old\n"
-            if stdout[0].decode() != ans :
+            if stdout[0].decode() != ans:
                 return False
             return True
     except Exception:
