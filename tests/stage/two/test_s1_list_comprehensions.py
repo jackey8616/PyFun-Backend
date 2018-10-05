018831394a4ffbb8583c519d37a91cb728fa88aa
Josh N <joshua.nowak@asu.edu>
2018-10-05 16:05:32 -0700

    Adds List Comprehensions to Stage two (#16)

---
+++ tests/stage/two/test_s1_list_comprehensions.py
@@ -0,0 +1,19 @@
+from stage.two.s1_list_comprehensions import route, data
+from tests.utils import check_attributes, post
+
+
+def test_attributes():
+    check_attributes(route, data)
+
+
+async def test_lesson(test_cli):
+    def override(res_data):
+        pass
+
+    req_data = {
+        'field_1': 'for',
+        'field_2': 'in',
+        'field_3': 'range',
+        'field_4': '10'
+    }
+    await post(cli=test_cli, url=route['url'], data=req_data, callback=override)
