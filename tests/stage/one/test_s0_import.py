871c999d915118f701c94f4177d3b03d98768129
SoliMark <zzzxxx804113401@gmail.com>
2018-10-11 18:55:48 +0800

    update

---
+++ tests/stage/one/test_s0_import.py
@@ -0,0 +1,14 @@
+from stage.one.s0_import import route, data
+from tests.utils import *
+
+
+def test_attributes():
+    check_attributes(route, data)
+
+
+async def test_lesson(test_cli):
+    await get(cli=test_cli, url=route['url'])
+    req_data = {
+        'field_1': 'import'
+    }
+    await post(cli=test_cli, url=route['url'], data=req_data)
