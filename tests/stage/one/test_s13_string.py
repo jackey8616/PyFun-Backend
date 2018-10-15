9e5347788e7d6bfabcee1aa84a905aedda526b64
stavhaygn <stavhaygnzhan@gmail.com>
2018-10-16 01:35:01 +0800

    Create stage1 s13_string (#42)

---
+++ tests/stage/one/test_s13_string.py
@@ -0,0 +1,19 @@
+from stage.one.s13_string import route, data
+from tests.utils import *
+
+
+def test_attributes():
+    check_attributes(route, data)
+
+
+async def test_lesson(test_cli):
+    await get(cli=test_cli, url=route['url'])
+    req_data = {
+            'field_1': 'len',
+            'field_2': 'split',
+            'field_3': 'replace',
+            'field_4': '41',
+            'field_5': '42',
+            'field_6': '43'
+    }
+    await post(test_cli, route['url'], data=req_data)
