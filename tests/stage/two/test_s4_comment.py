887f032b2ee5ed572b78df36e5c551c6d82ac936
stavhaygn <stavhaygnzhan@gmail.com>
2018-10-16 17:28:30 +0800

    Create stage2 s4_comment and remove index.html

---
+++ tests/stage/two/test_s4_comment.py
@@ -0,0 +1,16 @@
+from stage.two.s4_comment import route, data
+from tests.utils import *
+
+
+def test_attributes():
+    check_attributes(route, data)
+
+
+async def test_lesson(test_cli):
+    await get(cli=test_cli, url=route['url'])
+    req_data = {
+            'field_1': '#',
+            'field_2': '\'\'\'',
+            'field_3': '\'\'\'',
+    }
+    await post(test_cli, route['url'], data=req_data)
